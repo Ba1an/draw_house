12fb8857c09d3e9048015ca244ea46c43b2cbae4
karol <karolina_karo_balan@mail.ru>
2023-02-07 12:01:06 +0700

    Create function window()

---
+++ main.py
@@ -0,0 +1,15 @@
+def window():
+    """"
+    Function is drowing window.
+    :return: None
+    """
+
+    pass
+
+def home ():
+    """
+    Function is drowing home.
+    :return: None
+    """
+
+    pass
